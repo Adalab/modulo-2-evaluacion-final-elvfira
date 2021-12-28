d778b620c5f1abe4095a2f4b7af3a776048291f3
Elvira Cañas <elvfira@gmail.com>
2021-12-28 20:36:39 +0100

    js const

---
+++ js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//Defino constantes para los elementos
+const inputSearch = document.querySelector("js_narutoChapter");
+const searchButton = document.querySelector("js_searchButton");
+const resetButton = document.querySelector("js_resetChapter");
+const favoriteSeries = document.querySelector("js_favoriteSeries");
+const resultsSeries = document.querySelector("js_seriesResults");
+
+//Defino la función de búsqueda
+function searchNaruto () {
+
+};
+
+//Llamo a las funciones al clickar en los botones
+searchButton.addEventListener('click', searchNaruto);
