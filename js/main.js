a3f26b58d297485f271a09abffb0f7481909e213
Elvira Cañas <elvfira@gmail.com>
2022-01-02 23:39:31 +0100

    image not found

--- js/main.js
+++ js/main.js
@@ -7,10 +7,37 @@ const resetButton = document.querySelector(".js_resetChapter");
 const favoriteSeries = document.querySelector(".js_favoriteSeries");
 const resultsSeries = document.querySelector(".js_seriesResults");
 
+//Constante para las series que no tienen imagen
+const urlNoImage = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+const imageNotFound = 'https://cdn.myanimelist.net/images/qm_50.gif?s=e1ff92a46db617cb83bfc1e205aff620';
+
 //Defino variables
 let searchedSerie = "";
-let apiResults = [];
 let contentResults = "";
+let animeResults = {};
+let pasteFavorite = "";
+
+//Defino la función de elegir series favoritas
+function myFavorites(event) {
+  const selectFavorite = event.currentTarget;
+  // si el elemento selectFavorite contiene la clase selectedItem (comprobar si la contiene) haz algo...
+  if (selectFavorite.matches(".selectedItem")) {
+    // borra el elemento con la id correspondiente en el ul favoriteSeries
+    let chapterId = selectFavorite.getAttribute("id");
+    console.log(chapterId);
+    const elementToDelete = document.querySelector(
+      "ul.js_favoriteSeries #" + chapterId
+    );
+    console.log(elementToDelete);
+    elementToDelete.remove();
+  } else {
+    pasteFavorite = selectFavorite.cloneNode(true);
+    favoriteSeries.appendChild(pasteFavorite);
+  }
+  // favoriteSeries.innerHTML += selectFavorite;
+  // no funciona así que uso DOM
+  selectFavorite.classList.toggle("selectedItem");
+}
 
 //Defino la función de búsqueda
 function searchAnime(event) {
@@ -21,16 +48,36 @@ function searchAnime(event) {
     .then((response) => response.json())
     .then((data) => {
       const apiResults = data.results;
-      console.log(apiResults);
+      console.log(apiResults.length);
 
-      for (let i = 0; i <= apiResults.length; i++) {
-        let animeResults = apiResults[i];
-        resultsSeries.innerHTML += `<li>${animeResults.title}</li><img src="${animeResults.image_url}"/>`;
+      for (let i = 0; i <= apiResults.length - 1; i++) {
+        animeResults = apiResults[i];
+
+        //Sustituir imagen cuando la serie no tiene
+        let imageAddress = "";
+        if (animeResults.image_url != imageNotFound) {
+          imageAddress = animeResults.image_url;
+        } else {
+          imageAddress = urlNoImage;
+        }
+
+        //comprobar si este capítulo está en favoritos y ponerlo en verde (con un if)
+        let markFavorite = "";
+        // if (¿existe?) {
+        //   markFavorite = "selectedItem";
+        // } else {
+        // }
+
+        resultsSeries.innerHTML += `<li class="js_itemResult ${markFavorite}" id="chapter-${animeResults.mal_id}"><p>${animeResults.title}</p><img src="${imageAddress}"/></li>`;
+      }
+
+      const allResults = document.querySelectorAll(".js_itemResult");
+
+      for (const eachItem of allResults) {
+        eachItem.addEventListener("click", myFavorites);
       }
     });
 }
-
-
 
 //Llamo a las funciones al clickar en los botones
 searchButton.addEventListener("click", searchAnime);
